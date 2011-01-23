a5f9bcb7cb7a6ac6e9f8ced915c059131dfb303f
xaicron <xaicron@gmail.com>
2011-01-23 23:30:07 +0900

    initial commit

---
+++ lib/Kagura.pm
@@ -0,0 +1,42 @@
+package Kagura;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Kagura -
+
+=head1 SYNOPSIS
+
+  use Kagura;
+
+=head1 DESCRIPTION
+
+Kagura is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
