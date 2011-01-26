b83f63f3ff76d234d72d3b2a7e7bf0a095f5f2a5
xaicron <xaicron@gmail.com>
2011-01-27 02:00:35 +0900

    using Class::Accessor::Lite. default template Text::MicroTemplate.

--- lib/Kagura.pm
+++ lib/Kagura.pm
@@ -6,11 +6,15 @@ use Plack::Request;
 use Plack::Response;
 use Tiffany;
 use Encode ();
-use Object::Container ();
 use Router::Simple::Sinatraish ();
 use Class::Data::Inheritable;
-use Path::Class qw(file dir);
+use Path::Class qw/file dir/;
 use Plack::Util ();
+use Class::Accessor::Lite (
+    new => 1,
+    ro  => [qw/req/],
+    rw  => [qw/params stash/],
+);
 
 our $VERSION = '0.01';
 
@@ -33,12 +37,6 @@ sub import {
     };
 }
 
-sub new {
-    my $class = shift;
-    my %args = @_ == 1 ? %{$_[0]} : @_;
-    bless { %args }, $class;
-}
-
 sub contenxt { die "no context is awaked" }
 
 sub mk_classdata {
@@ -54,12 +52,11 @@ sub init {
     $class->mk_classdata('response_class');
     $class->mk_classdata('request_class');
     $class->mk_classdata('renderer');
-    $class->mk_classdata('container');
 
     $class->init_home_dir();
     $class->init_config();
     $class->init_renderer();
-    $class->init_container();
+    $class->init_plugins();
     $class->response_class('Plack::Response');
     $class->request_class('Plack::Request');
 }
@@ -81,33 +78,32 @@ sub init_config {
 sub init_renderer {
     my ($class) = @_;
 
-    my $config    = $class->config->{template};
-    my $path      = $class->home_dir->subdir($config->{path} || 'tmpl');
-    my $cache_dir = $path->subdir($config->{cache_dir} || 'cache');
+    my $config = $class->config->{template};
+    my $path   = do {
+        my $path = $config->{path} || ['tmpl'];
+        $path = [ $path ] unless ref $path eq 'ARRAY';
+        $path;
+    };
 
-    my $renderer = Tiffany->load('Text::Xslate', {
-        syntax    => $config->{syntax} || 'TTerse',
-        path      => $path->stringify,
-        module    => [ 'Text::Xslate::Bridge::TT2Like', @{ $config->{module} || [] } ],
-        cache     => $config->{cache} || 1,
-        cache_dir => $cache_dir->stringify,
+    my $renderer = Tiffany->load('Text::MicroTemplate::File', {
+        include_path => [ map { $class->home_dir->subdir($_)->stringify } @$path ],
+        use_cache    => $config->{use_cache},
     });
+
     $class->renderer($renderer);
 }
 
-sub init_container {
+sub init_plugins {
     my ($class) = @_;
-    $class->container('Object::Container');
-    return unless $class->config->{container};
+    my $config = $class->config->{plugins} || [];
+    $class->load_plugins(@$config);
+}
 
-    my $container = $class->config->{container};
-    for my $name (keys %$container) {
-        $class->container->register({
-            class       => $name,
-            initializer => $container->{$name}{init},
-            args        => $container->{$name}{args},
-            preload     => $container->{$name}{preload},
-        });
+sub load_plugins {
+    my ($class, @args)  = @_;
+    for (my $i = 0; $i < @args; $i+=2) {
+        my ($module, $conf) = @args[$i,$i+1];
+        $class->load_plugin($module, $conf);
     }
 }
 
@@ -117,17 +113,8 @@ sub load_plugin {
     $module->init($conf);
 }
 
-sub load_plugins {
-    my ($class, @args)  = @_;
-    my $conf = $class->config->{plugin} || {};
-    for (my $i = 0; $i < @args; $i+=2) {
-        my ($module, $conf) = @args[$i,$i+1];
-        $class->load_plugin($module, $conf);
-    }
-}
-
 sub render {
-    my ($self) = shift;
+    my $self = shift;
     my $content = Encode::encode_utf8($self->renderer->render(@_));
     $self->response_class->new(200, [
         'Content-Length' => length($content),
@@ -136,23 +123,19 @@ sub render {
 }
 
 sub return_404 {
-    return shift->response_class->new(404, [], ['404 not found']);
+    return $_[0]->response_class->new(404, [], ['404 not found']);
 }
 
 sub return_403 {
-    return shift->response_class->new(403, [], ['403 forbidden']);
+    return $_[0]->response_class->new(403, [], ['403 forbidden']);
 }
 
 sub show_error {
     my ($self, $msg) = @_;
-    return shift->response_class->new(500,
+    return $self->response_class->new(500,
         ['Content-Type' => 'text/html; charset=utf-8'],
         [$msg || ''],
     );
-}
-
-sub req {
-    $_[0]->{req};
 }
 
 sub model {
@@ -161,10 +144,10 @@ sub model {
     $self->container->get($model_class);
 }
 
-sub params {
-    my $self = shift;
-    return $self->{params} unless @_;
-    $self->{params} = shift;
+sub dispatch {
+    my ($class, $route, $method) = @_;
+    my $module = Plack::Util::load_class($route, "$class\::Web::C");
+    return \&{"$module\::$method"};
 }
 
 sub to_app {
@@ -176,7 +159,7 @@ sub to_app {
     sub {
         my $env = shift;
         my $req = $pkg->request_class->new($env);
-        my $c   = $pkg->new(req => $req, class => $class);
+        my $c   = $pkg->new(req => $req, class => $class, stash => {});
         no strict 'refs';
         local *{"$pkg\::context"} = sub { $c };
         use strict 'refs';
@@ -185,18 +168,12 @@ sub to_app {
             my $code = delete $route->{code};
             $c->params({ %$route });
             my $res = $code->($c, $req);
-            $res->finalize;
+            return $res->finalize;
         }
         else {
             return [404, [], ['not found']];
         }
     };
-}
-
-sub dispatcher {
-    my ($class, $route, $method) = @_;
-    my $module = Plack::Util::load_class($route, "$class\::C");
-    return \&{"$module\::$method"};
 }
 
 1;
